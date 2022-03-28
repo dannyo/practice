68d86705b688f26454441d7590f6144a8a6416c5
dannyo <dannyorh@gmail.com>
2022-03-27 17:41:37 -0700

    fixing division logic

--- js/division.js
+++ js/division.js
@@ -1,9 +1,9 @@
 // function that divides x by y e.g (x/y) without using '/' operator
 var divide = function(x,y) {
         var count = 0;
-        while (x > y){
+        while (x >= y){
             count++;
             x-=y;
         }
-        return x;
+        return count;
     };
